bbd85a5a232b8621d9b0b471e957beae765f8197
Andrew Radev (r0978323) <r0978323@student.kuleuven.be>
2024-10-27 16:51:51 +0100

    Fix issues with PCA script

--- scripts/build_nmd_file.py
+++ scripts/build_nmd_file.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from prody import parseDCD, parsePDB, writeNMD
+from pathlib import Path
+from prody import parseDCD, parsePDB, writeNMD, EDA
 
 if len(sys.argv) < 4:
     print(f"USAGE: python {os.path.basename(__file__)} <protein.pdb> <protein.dcd> <output.nmd>")
@@ -10,6 +11,8 @@ pdb_file    = sys.argv[1]
 dcd_file    = sys.argv[2]
 output_file = sys.argv[3]
 
+protein_name = Path(pdb_file).stem
+
 ensemble = parseDCD(dcd_file)
 num_atoms = ensemble.numAtoms()
 structure = parsePDB(pdb_file).select('index 1 to %d' % num_atoms)
@@ -18,6 +21,6 @@ ensemble.setCoords(structure)
 ensemble.setAtoms(structure)
 ensemble.superpose()
 
-eda_ensemble = EDA('%s Ensemble' % wildcards.protein_name)
+eda_ensemble = EDA('%s Ensemble' % protein_name)
 eda_ensemble.buildCovariance(ensemble)
 eda_ensemble.calcModes(n_modes=10) # can do more but will take longer
