d56a3d7576834163d4d67cd98493d82b56284143
Andrew Radev <andrey.radev@gmail.com>
2024-11-12 21:01:17 +0100

    Make extension print information from a hardcoded file

--- blender/extension/__init__.py
+++ blender/extension/__init__.py
@@ -1,11 +1,5 @@
-# this script is just a skeleton for the blender plugin assuming we would have added operators for each
-# of the fragmentation methods instead needing to process the data before importing to blender
-
-# bl_info = {
-#     "name": "Protein Runway",
-#     "blender": (2, 80, 0),
-#     "category": "Object",
-# }
+import os
+from pathlib import Path
 
 import bpy
 import MDAnalysis as mda
@@ -23,28 +17,27 @@ class RenderTrajectory(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        # The original script
-        scene = context.scene
-        for obj in scene.objects:
-            continue
+        root_dir = Path(os.path.dirname(__file__))
 
-        print(MDAnalysis.__file__)
+        u = mda.Universe(root_dir / 'tmp_data/1fuu_noPTM.nmd_traj.pdb')
+
+        message = f"Atoms: {len(u.atoms)}, Frames: {len(u.trajectory)}"
+        def draw(self, context):
+            self.layout.label(text=message)
+        bpy.context.window_manager.popup_menu(draw, title="Example", icon="INFO")
 
         return {'FINISHED'}
+
 
 
 def menu_func(self, context):
     self.layout.operator(RenderTrajectory.bl_idname)
 
+
 def register():
     bpy.utils.register_class(RenderTrajectory)
-    bpy.types.VIEW3D_MT_object.append(menu_func)
+    bpy.types.TOPBAR_MT_edit.append(menu_func)
+
 
 def unregister():
     bpy.utils.unregister_class(RenderTrajectory)
-
-
-# This allows you to run the script directly from Blender's Text editor
-# to test the add-on without having to install it.
-if __name__ == "__main__":
-    register()
