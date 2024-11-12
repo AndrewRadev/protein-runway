320916fcbbf2da6e90cbbd046a534e34e572a512
Andrew Radev <andrey.radev@gmail.com>
2024-11-12 21:36:26 +0100

    Add a panel with a file input

--- blender/extension/__init__.py
+++ blender/extension/__init__.py
@@ -4,24 +4,34 @@ from pathlib import Path
 import bpy
 import MDAnalysis as mda
 
+bpy.types.Scene.ProteinRunway_local_path = bpy.props.StringProperty(
+    name="File",
+    description="File path of the PDB with an embedded trajectory to open",
+    options={"TEXTEDIT_UPDATE"},
+    subtype="FILE_PATH",
+    maxlen=0,
+)
 
-class RenderTrajectory(bpy.types.Operator):
-    """
-    [TODO] Open a PDB that contains a trajectory and add that to the scene as
-    an object with an animation.
-    """
-    bl_idname = "object.trajectory"
-    bl_label = "Render Trajectory"
+class ImportTrajectoryOperator(bpy.types.Operator):
+    "Load the PDB"
+
+    bl_idname = "protein_runway.import_trajectory"
+    bl_label = "Import"
 
     # Enable undo for the operator.
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        root_dir = Path(os.path.dirname(__file__))
+        scene = context.scene
+        file_path = scene.ProteinRunway_local_path
 
-        u = mda.Universe(root_dir / 'tmp_data/1fuu_noPTM.nmd_traj.pdb')
+        u = mda.Universe(file_path)
 
+        # For now, just print a message:
         message = f"Atoms: {len(u.atoms)}, Frames: {len(u.trajectory)}"
+        self.report({'INFO'}, message)
+
+        # Also render a panel with information:
         def draw(self, context):
             self.layout.label(text=message)
         bpy.context.window_manager.popup_menu(draw, title="Example", icon="INFO")
@@ -29,14 +39,31 @@ class RenderTrajectory(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class ImportTrajectoryPanel(bpy.types.Panel):
+    bl_label = "Import Trajectory"
+    bl_idname = "PROTEINRUNWAY_PT_import_panel"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "scene"
 
-def menu_func(self, context):
-    self.layout.operator(RenderTrajectory.bl_idname)
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        row = layout.row(align=True)
+
+        layout.label(text="Load a PDB File with a trajectory", icon="FILE_TICK")
+        layout.separator()
+
+        row_file = layout.row()
+        row_file.prop(scene, "ProteinRunway_local_path")
+
+        row_button = layout.row()
+        row_button.operator(ImportTrajectoryOperator.bl_idname)
 
 
 def register():
-    bpy.utils.register_class(RenderTrajectory)
-    bpy.types.TOPBAR_MT_edit.append(menu_func)
+    bpy.utils.register_class(ImportTrajectoryOperator)
+    bpy.utils.register_class(ImportTrajectoryPanel)
 
 
 def unregister():
