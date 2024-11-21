f802bdd4ca15d975a264a0fe8759c335737246b7
Andrew Radev <andrey.radev@gmail.com>
2024-11-21 18:19:49 +0100

    Add a note

--- scripts/generate_amsm.R
+++ scripts/generate_amsm.R
@@ -9,6 +9,10 @@ if (length(args) < 2) {
 traj      <- read.dcd(args[1])
 amsm_path <- args[2]
 
+# We could just use amsm.xyz here, but it doesn't fit the atom positions before
+# measuring movement similarity, so let's run the full function and extract the
+# matrix. The actual clustering is fast enough that the wasted work doesn't
+# matter.
 clustering <- geostas(traj, fit=T, pairwise=T)
 
 write.table(clustering$amsm, sep=',', amsm_path, row.names=F, col.names=F)
