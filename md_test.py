020e899ebe5edca44eeb97b8619aa8fdd46db079
Andrew Radev <andrey.radev@gmail.com>
2024-11-21 16:56:51 +0100

    Update md_test.py

--- md_test.py
+++ md_test.py
@@ -1,27 +1,19 @@
 import MDAnalysis as mda
 
 # Load simulation results with a single line
-u = mda.Universe('data/pdb/1fuu_noPTM_complex.top', 'data/traj/1fuu_noPTM_10-20ns_100snap.trr')
+u = mda.Universe('01_input/top/1fuu_noPTM_complex.top', '01_input/traj/1fuu_noPTM_10-20ns_100snap.trr')
+# u = mda.Universe('1a3w_noPTM.nmd_traj.pdb.gz')
 
 print(u)
 
 # Select atoms
-ag = u.select_atoms('name OH')
-print("Atoms with name OH")
+ag = u.select_atoms('protein')
 print(ag)
 
-# Atom data made available as Numpy arrays
-print("Positions")
-print(ag.positions)
-
-# print("Velocities")
-# print(ag.velocities)
-# Error: MDAnalysis.exceptions.NoDataError: This Timestep has no velocities
-
-# print("Forces")
-# print(ag.forces)
-# Error: MDAnalysis.exceptions.NoDataError: This Timestep has no forces
-
-# Iterate through trajectories
-for ts in u.trajectory:
-    print(ag.center_of_mass())
+# # Atom data made available as Numpy arrays
+# print("Positions")
+# print(ag.positions)
+#
+# # Iterate through trajectories
+# for ts in u.trajectory:
+#     print(ag.center_of_mass())
