b9a8babf7006b4f7227ac9ed405c051658fa4ed7
Andrew Radev <andrey.radev@gmail.com>
2024-12-08 14:59:22 +0100

    Generate the documentation site from this branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,6 @@ autosummary_generate = True  # Turn on sphinx.ext.autosummary
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
